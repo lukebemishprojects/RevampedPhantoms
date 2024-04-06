7b271d0ee26e27eae5324bf65f3c4984ae6a94a1
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-06 01:36:40 -0500

    Spotless

--- neoforge/src/main/java/dev/lukebemish/revampedphantoms/neoforge/client/ModClient.java
+++ neoforge/src/main/java/dev/lukebemish/revampedphantoms/neoforge/client/ModClient.java
@@ -2,13 +2,12 @@ package dev.lukebemish.revampedphantoms.neoforge.client;
 
 import dev.lukebemish.revampedphantoms.client.ClientPlatform;
 import dev.lukebemish.revampedphantoms.neoforge.ModEntrypoint;
+import java.util.Objects;
+import java.util.function.Supplier;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.neoforged.neoforge.client.event.EntityRenderersEvent;
-
-import java.util.Objects;
-import java.util.function.Supplier;
 
 public final class ModClient implements ClientPlatform {
 	public static final ModClient INSTANCE = new ModClient();
